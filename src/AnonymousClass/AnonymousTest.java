58f2b2ef5c2cac3bf665c5850f9137c619910da0
abdurrahman4127 <71093608+abdurrahman4127@users.noreply.github.com>
2022-03-29 23:13:40 +0600

    got rid of her name

--- src/AnonymousClass/AnonymousTest.java
+++ src/AnonymousClass/AnonymousTest.java
@@ -19,7 +19,7 @@ public class AnonymousTest {
     public static void main(String[] args)
     {
         // anonymous class
-        Person p = new Person("Thao Nhi") {
+        Person p = new Person("Abdur Rahman") {
             public void display() {
                 display(2); // this is fine
                 System.out.println("Hello from Anonymous");
@@ -32,7 +32,7 @@ public class AnonymousTest {
         };
 
         p.display();
-        p.display("Vo"); // Ok
+        p.display("Siuuuu"); // Ok
 //        p.display(3); // error: The method display(String) in the type Person is not applicable for the arguments (int)
 
     }
